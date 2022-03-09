eb6ae4d6b13e1479add584c20056cdc727b3f662
smllygrl <hodgsonatalie@gmail.com>
2022-03-09 16:12:53 +1100

    Made a bank with CSharp 101 by dotNET

---
+++ CSpractice/BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSpractice
+{
+    public class BankAccount
+    {
+            public string Number { get; }
+            public string Owner { get; set; }
+            public decimal Balance {
+            get {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+            private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            // can't deposit negative money
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            // can't withdraw negative money
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            // purchase cannot leave negative balance
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this withdrawl");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+    }
+}
--- CSpractice/Program.cs
+++ CSpractice/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using System; // System.Console ... helps access Console
+using System.Collections.Generic;
 
-namespace CSpractice
+namespace CSpractice 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            var myAccount = new BankAccount("Natalie", 1000);
+
+            Console.WriteLine($"Account {myAccount.Number} was created for {myAccount.Owner} with {myAccount.Balance}");
+
+            myAccount.MakeWithdrawl(120, DateTime.Now, "STAX");
+
+            myAccount.MakeWithdrawl(300, DateTime.Now, "Laptop Repairs");
+
+            Console.WriteLine(myAccount.GetAccountHistory());
+
+
+
+            ////Test that there is not a negative balance
+            //myAccount.MakeDeposit(-300, DateTime.Now, "trying to steal");
+
+            ////Test that initial balances must be positive.
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException error)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(error.ToString());
+            //}
+
+
+
         }
     }
 }
